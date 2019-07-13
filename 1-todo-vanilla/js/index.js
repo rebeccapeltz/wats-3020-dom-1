cf1841bdd15b9f73cb034e2c17528bfa7e46c74c
rebeccapeltz <rebeccapeltz@gmail.com>
2019-07-12 20:51:57 -0700

    vanilla

--- 1-todo-vanilla/js/index.js
+++ 1-todo-vanilla/js/index.js
@@ -1,21 +1,23 @@
-//TODO add event to listen to when DOM is ready to access
-document.addEventListener("", event => {
-  //TODO add event listener to signal that user is submitting form
+document.addEventListener("DOMContentLoaded", event => {
 
-  //TODO call a function to prevent the click from "bubbling up"
+  document.addEventListener("submit", event => {
+    event.preventDefault()
 
-  //TODO add a selector that selects the input element with the attribute
-  //name equal to "item-input"
-    let itemInput = document.querySelector();
+    let itemInput = document.querySelector("input[name=item-input]");
 
     let itemValue = itemInput.value;
     //test to be sure that user entered a value
     if (itemValue.length !== 0) {
       //create new item
-
-      //TODO create a list item with a checkbox and a label containing the 
+      //create a list item with a checkbox and a label containing the 
       //user input string
-      
+      let newItemEl = document.createElement('li');
+      let newCheckBox = document.createElement('input')
+      newCheckBox.setAttribute("type", "checkbox")
+      newItemEl.appendChild(newCheckBox)
+      let newLabel = document.createElement('label')
+      newLabel.innerHTML = itemValue
+      newItemEl.appendChild(newLabel)
 
       //get a reference to list and append list item
       let list = document.querySelector('.todo-list');
@@ -24,7 +26,6 @@ document.addEventListener("", event => {
       itemInput.value = '';
 
       //add a click event listener that serves a toggle
-      //TODO add click event toggle that strikes out text in label associated with list item
       newCheckBox.addEventListener("click", function (event) {
         if (this.nextSibling.style.textDecoration === "line-through") {
           this.nextSibling.style.textDecoration = ""
@@ -33,4 +34,5 @@ document.addEventListener("", event => {
         }
       })
     }
+  })
 })
